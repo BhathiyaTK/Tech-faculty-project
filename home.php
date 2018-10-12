11571cdfde1d4b1fcb6d4b484cb8505395b99a1d
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2018-10-12 23:54:22 +0530

    Home page for select client action

---
+++ home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" type="text/css" href="home-page-style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<title>Inventory Management System</title>
+</head>
+<body>
+	<section>
+		<div id="home-page-content">
+			<div class="home-page-title">
+				<h1>Technology Faculty - SUSL</h1>
+				<h3>Inventory Management System</h3>
+			</div>
+			<div class="home-page-details">
+				<div class="row">
+					<div class="button-div col-lg-6">
+						<div class="home-page-buttons">
+							<a href=""><i class="fas fa-search"></i>Check</a>
+							<p>
+								Check inventory details on the system
+							</p>
+						</div>
+					</div>
+					<div class="button-div col-lg-6">
+						<div class="home-page-buttons">
+							<a href="inventory-form.php"><i class="fas fa-plus"></i>Add</a>
+							<p>
+								Add new inventory into the system
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
