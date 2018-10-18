1d0478ca0d35e9bc4e9525ca1611e5e177d95206
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2018-10-18 22:52:27 +0530

    added new db table for enter details of new inventory type

--- inventory_submission.sql
+++ inventory_submission.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 18, 2018 at 05:11 PM
+-- Generation Time: Oct 18, 2018 at 05:20 PM
 -- Server version: 5.7.19
 -- PHP Version: 7.1.9
 
@@ -41,6 +41,23 @@ CREATE TABLE IF NOT EXISTS `inventory_submission` (
   `Sub Inventory Item` varchar(100) NOT NULL,
   `Price` varchar(10000) NOT NULL,
   `Quantity` varchar(10000) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `new_inventory`
+--
+
+DROP TABLE IF EXISTS `new_inventory`;
+CREATE TABLE IF NOT EXISTS `new_inventory` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `Code1` varchar(200) NOT NULL,
+  `Main_location` varchar(200) NOT NULL,
+  `Sub_location` varchar(200) NOT NULL,
+  `Main_inventory_type` varchar(200) NOT NULL,
+  `Inventory_name` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 COMMIT;
