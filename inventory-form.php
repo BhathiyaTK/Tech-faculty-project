8c65fa3871e295927a69147801799410d08b71ba
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2018-10-19 13:00:10 +0530

    stablished db connection with form page

--- inventory-form.php
+++ inventory-form.php
@@ -1,3 +1,17 @@
+<?php
+
+$host="localhost";
+$db_name="root";
+$db_pass= "";
+$db="techfaculty_inventory";
+
+$conn = new mysqli($host,$db_name,$db_pass,$db);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
