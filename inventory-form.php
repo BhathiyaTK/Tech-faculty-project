307dafdcbb4497fc997e22949781017d12f66ae8
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2018-10-19 12:50:12 +0530

    added js function for only enter numbers in quantity text field

--- inventory-form.php
+++ inventory-form.php
@@ -153,6 +153,16 @@
 			      	form.submit();
 			    }
             });
+            $("#quantity").keydown(function (e) {
+		        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110]) !== -1 ||
+		            (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+		            (e.keyCode >= 35 && e.keyCode <= 40)) {
+		                 return;
+		        }
+		        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+		            e.preventDefault();
+		        }
+		    });
 		});
 
 		$(function(){
