2541498108c1cdd9d8cbf44ac56eebbf9afa72e1
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2018-10-26 19:19:23 +0530

    added form structures

--- check-inventory.php
+++ check-inventory.php
@@ -1,3 +1,18 @@
+<?php
+
+$host="localhost";
+$db_name="root";
+$db_pass= "";
+$db="techfaculty_inventory";
+
+$conn = new mysqli($host,$db_name,$db_pass,$db);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,6 +31,41 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
 	<title>Check Inventory</title>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('#check_inventory').click(function(){
+				var check_id = $("#check_inventory").val();
+				var location_id = $("#location").val();
+				var inventory_id = $("#inventory").val();
+				$.post(
+					"fetch_details.php",
+					{
+						checkId:check_id, 
+						locationId:location_id, 
+						inventoryId:inventory_id
+					},
+					function(data)
+					{
+						$('#details_content').html(data);
+					}
+				);
+			});
+			$("#check_form").validate({
+	        	rules: {
+	        		location: "required",
+	        		inventory: "required"
+	        	},
+	    		messages: {
+	    			location: "Please select the location",
+			      	inventory: "Please select inventory"
+			    },
+
+			    submitHandler: function(form) {
+			      	form.submit();
+			    }
+	        });
+		});
+	</script>
 </head>
 <body>
 	<section>
@@ -24,6 +74,49 @@
 				<h1>Check Inventory Details</h1>
 			</div>
 			<div class="form-divider"></div>
+			<div id="check-form-content">
+				<form id="check_form">
+					<div class="form-row">
+					    <div class="form-group col-sm-12 col-md-6 col-lg-6 sub-form-row">
+					    	<legend>Location</legend>
+					    	<select id="location" class="form-control" name="location">
+					    		<option value="">Choose...</option>
+					    		<option value="A">Old Building</option>
+					    		<option value="B">New Building</option>
+					    		<option value="A.1">Department of Engineering Technology</option>
+					    		<option value="B.3">Department of Bio System</option>
+					    		<option value="all">All</option>
+					      	</select>	
+					    </div>
+					    <div class="form-group col-sm-12 col-md-6 col-lg-6 sub-form-row-left">
+						    <legend for="inputState">Inventory Type</legend>
+					      	<select id="inventory" class="form-control" name="inventory">
+					      		<option value="">Choose...</option>
+					      		<?php
+
+					    		$sql_main_inventory = "SELECT * FROM main_inventory_item";
+					    		$main_inv_results = mysqli_query($conn,$sql_main_inventory);
+
+					    		while ($row = mysqli_fetch_array($main_inv_results)) {
+					    			echo "<option value=".$row['main_inventory_val'].">".$row['main_inventory_item']."</option>";
+					    		}
+
+					    		?>
+					      	</select>
+					    </div>
+					</div>
+					<div class="form-divider-bottom"></div>
+					<div id="check-button-div">
+						<button type="submit" name="check" id="check_inventory" class="btn btn-success">Check Details<i class="fas fa-search"></i></button>
+					</div>
+				</form>
+			</div>
+			<div id="details_content">
+				
+			</div>
+		</div>
+		<div id="back-to-home-button">
+			<a class="btn btn-danger" href="home.php">Back to Home</a>
 		</div>
 	</section>
 </body>
