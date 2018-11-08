341845d3d18066ebcbbdf3014baaab75aaf17e63
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2018-11-08 21:23:49 +0530

    added images into centered divs

--- home.php
+++ home.php
@@ -29,6 +29,9 @@
 					<div class="row">
 						<div class="button-div col-lg-4">
 							<div class="home-page-buttons" id="home-page-buttons-left">
+								<p>
+									<img src="images/research.png">
+								</p>
 								<a href="check-inventory.php"><i class="fas fa-search"></i>Check</a>
 								<p>
 									Check inventory details
@@ -37,7 +40,10 @@
 						</div>
 						<div class="button-div col-lg-4">
 							<div class="home-page-buttons" id="home-page-buttons-center">
-								<a href="inventory-form.php"><i class="fas fa-share-square"></i>Add</a>
+								<p>
+									<img src="images/input.png">
+								</p>
+								<a href="inventory-form.php"><i class="fas fa-share-square"></i>add</a>
 								<p>
 									Submit existing inventory
 								</p>
@@ -45,6 +51,9 @@
 						</div>
 						<div class="button-div col-lg-4">
 							<div class="home-page-buttons" id="home-page-buttons-right">
+								<p>
+									<img src="images/add-file.png">
+								</p>
 								<a href="add-inventory.php"><i class="fas fa-plus"></i>New</a>
 								<p>
 									Add new inventory
